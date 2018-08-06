bd5790866ed5d575cbb7659602dbcbf70f99840c
congvan <congvan203@gmail.com>
2018-08-06 10:39:26 +0700

    Add Source

---
+++ DB/dump.sql
@@ -0,0 +1,57 @@
+﻿--CREATE DATABASE ManageCourse;
+--GO
+USE ManageCourse;
+GO
+CREATE TABLE [User]
+(ID       INT IDENTITY(1, 1) PRIMARY KEY, 
+ Name     VARCHAR(100), --ten dang nhap
+ PassWord VARCHAR(100), --
+ Type     INT--1:Admin, 2:Teacher, 3: Student
+);
+GO
+CREATE TABLE Student
+(ID         INT IDENTITY(1, 1) PRIMARY KEY, --ID
+ Code       INT, --Mã sinh viên
+ FullName   NVARCHAR(100), --Tên sinh viên
+ Gender     TINYINT, --1: Nam, 2: Nu
+ DayOfBirth DATE, --Ngày sinh
+ Address    NVARCHAR(500), --Địa chỉ
+ MajorID    INT, --ID Chuyên ngành
+ UserID     INT, --ID người dùng
+);
+GO
+CREATE TABLE Major--Bảng chuyên ngành
+(ID            INT IDENTITY(1, 1) PRIMARY KEY, 
+ Code          INT, --Mã chuyên ngành
+ Name          NVARCHAR(200), --Tên chuyên ngành
+ TotalStudied  INT, --Tổng số sinh viên theo học
+ NumberTopical INT, -- số chuyên đề bắt buộc của mỗi chuyên ngành
+);
+GO
+CREATE TABLE Topical -- Chuyên đề
+(ID            INT IDENTITY(1, 1) PRIMARY KEY, 
+ Code          INT, --Mã chuyên đề
+ Name          NVARCHAR(200), --Tên chuyên đề
+ --NumberClass INT,--Số sinh viên tối đa của mỗi chuyên đề
+ Deadline      DATETIME, --Deadline của mỗi chuyên đề
+ NumberTeam    INT, --Số lượng nhóm
+ NumberStudent INT, --Số lượng học sinh
+ Enable        BIT, --Trạng thái chuyên đề// True:mở - False: đóng
+ MajorID       INT--ID chuyên ngành (chuyên đề thuộc ngành nào)
+);
+GO
+CREATE TABLE [Subject]--Môn học
+(ID        INT IDENTITY(1, 1) PRIMARY KEY, 
+ Name      NVARCHAR(200), --Tên môn học
+ TopicalID INT, --ID chuyên đề(Môi chuyên đề có nhiều môn học)
+ Enable bit
+);
+CREATE TABLE Student_Subject-- Sinh viên đăng ký môn học 
+(ID        INT IDENTITY(1, 1) PRIMARY KEY, 
+ StudentID INT, --ID sinh viên
+ SubjectID INT, --ID môn họcs
+ Session   INT, --Học kỳ 
+ Point     DECIMAL--Điểm
+
+);
+GO
---
+++ ManageCourse/Login/Login.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Login
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ĐĂNG NHẬP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên đăng nhập";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(53, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mật khẩu";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(134, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(134, 118);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(208, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Coral;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(134, 163);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(102, 34);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.White;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(242, 163);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(102, 34);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(361, 216);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Coral;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập vào hệ thống";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
