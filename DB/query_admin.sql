9efdca82c82a6abe24a9791ad59d81419797efa2
congvan <congvan203@gmail.com>
2018-08-09 12:12:51 +0700

    add query admin

---
+++ DB/query_admin.sql
@@ -0,0 +1,187 @@
+﻿USE ManageCourse;
+GO
+CREATE PROC [dbo].[DeleteMajor] @ID INT
+AS
+     BEGIN
+         DELETE dbo.Major
+         WHERE dbo.Major.ID = @ID;
+     END;
+GO
+CREATE PROC [dbo].[DeleteUser] @ID INT
+AS
+     BEGIN
+         DECLARE @Type INT;
+         SELECT @Type = u.Type
+         FROM dbo.[User] u
+         WHERE u.ID = @ID;
+         DELETE dbo.[User]
+         WHERE ID = @ID;
+         IF @Type = 3
+             BEGIN
+                 DELETE dbo.Student
+                 WHERE dbo.Student.UserID = @ID;
+             END;
+     END;
+GO
+CREATE PROC [dbo].[GetAllUser]
+AS
+     BEGIN
+         SELECT ID, UserName,
+                    CASE Type
+                        WHEN 1 THEN N'Admin'
+                        WHEN 2 THEN N'Giáo viên'
+                        WHEN 3 THEN N'Sinh viên'
+                        ELSE ''
+                    END 'Type',
+                        CASE Enable
+                            WHEN 1 THEN N'Kích hoạt'
+                            ELSE N'Hủy'
+                        END 'Enable', FromDate, ToDate
+         FROM [User];
+     END;
+GO
+CREATE PROC [dbo].[GetStudentFromUser] @ID INT
+AS
+     BEGIN
+         SELECT s.ID, s.Code, s.FullName, s.Gender, CONVERT(VARCHAR(20), s.DayOfBirth, 111) DayOfBirth, s.Address, s.MajorID, s.UserID
+         FROM dbo.Student s
+         WHERE s.UserID = @ID;
+     END;
+GO
+CREATE PROC [dbo].[GetUser] @UserName VARCHAR(100), 
+                            @PassWord VARCHAR(100)
+AS
+     BEGIN
+         SELECT u.UserName, u.PassWord, u.Type, CONVERT(VARCHAR(20), u.FromDate, 111) FromDate, CONVERT(VARCHAR(20), u.ToDate, 111) ToDate
+         FROM [dbo].[User] u
+         WHERE u.UserName = @UserName
+               AND u.PassWord = @PassWord;
+     END;
+GO
+CREATE PROC [dbo].[GetUserFromID] @ID INT
+AS
+     BEGIN
+         SELECT u.UserName, u.PassWord, u.Enable, u.Type, CONVERT(VARCHAR(20), u.FromDate, 111) FromDate, CONVERT(VARCHAR(20), u.ToDate, 111) ToDate
+         FROM [dbo].[User] u
+         WHERE u.ID = @ID;
+     END;
+GO
+USE [ManageCourse];
+GO
+CREATE PROC [dbo].[InsertMajor] @Name          NVARCHAR(200), 
+                                @NumberTopical INT
+AS
+     BEGIN
+         DECLARE @Code VARCHAR(100)= 'MJ';
+         SELECT @Code = @Code + CAST(ISNULL(MAX(m.ID), 1) AS VARCHAR(10))
+         FROM dbo.Major m;
+         INSERT INTO dbo.Major
+         (
+         --ID - this column value is auto-generated
+         Code, Name, TotalStudied, NumberTopical
+         )
+         VALUES
+         (
+         -- ID - int
+         @Code, -- Code - varchar
+         @Name, -- Name - nvarchar
+         @NumberTopical, -- TotalStudied - int
+         0 -- NumberTopical - int
+         );
+     END;
+GO
+CREATE PROC [dbo].[InsertUser] @UserName   VARCHAR(100), 
+                               @PassWord   VARCHAR(100), 
+                               @Type       INT, 
+                               @Enable     BIT, 
+                               @FromDate   DATETIME, 
+                               @ToDate     DATETIME, 
+                               @FullName   NVARCHAR(100), 
+                               @Gender     TINYINT, 
+                               @DayOfBirth DATE, 
+                               @Address    NVARCHAR(500)
+AS
+     BEGIN
+         INSERT INTO dbo.[User]
+         (
+         --ID - this column value is auto-generated
+         UserName, PassWord, Type, Enable, FromDate, ToDate
+         )
+         VALUES
+         (
+         -- ID - INT
+         @UserName, -- UserName - VARCHAR
+         @PassWord, -- PassWord - VARCHAR
+         @Type, -- Type - INT
+         @Enable, -- Enable - BIT
+         @FromDate, -- FromDate - DATETIME
+         @ToDate -- ToDate - DATETIME
+         );
+         DECLARE @IDUser INT= 0;
+         SELECT @IDUser = SCOPE_IDENTITY();
+         IF @IDUser > 0
+            AND @Type = 3
+             BEGIN
+                 DECLARE @CodeStudent VARCHAR(100);
+                 SET @CodeStudent = 'SV'+CAST(@IDUser AS VARCHAR(10));
+                 INSERT INTO dbo.Student(Code, FullName, Gender, DayOfBirth, Address, UserID)
+                 VALUES
+                 (@CodeStudent, -- Code - 
+                  @FullName, -- FullName - NVARCHAR
+                  @Gender, -- Gender - TINYINT
+                  @DayOfBirth, -- DayOfBirth - DATE
+                  @Address, -- Address - NVARCHAR
+                  @IDUser -- UserID - INT
+                 );
+             END;
+     END;
+GO
+CREATE PROC [dbo].[UpdateMajor] @Name          NVARCHAR(200), 
+                                @NumberTopical INT, 
+                                @ID            INT
+AS
+     BEGIN
+         UPDATE dbo.Major
+           SET 
+               dbo.Major.Name = @Name, -- nvarchar
+               dbo.Major.NumberTopical = @NumberTopical -- int
+         WHERE dbo.Major.ID = @ID;
+     END;
+GO
+CREATE PROC [dbo].[UpdateUser] @UserName   VARCHAR(100), 
+                               @PassWord   VARCHAR(100), 
+                               @Type       INT, 
+                               @Enable     BIT, 
+                               @FromDate   DATETIME, 
+                               @ToDate     DATETIME, 
+                               @FullName   NVARCHAR(100), 
+                               @Gender     TINYINT, 
+                               @DayOfBirth DATE, 
+                               @Address    NVARCHAR(500), 
+                               @ID         INT
+AS
+     BEGIN
+         UPDATE dbo.[User]
+           SET 
+               UserName = @UserName, -- VARCHAR
+               PassWord = @PassWord, -- VARCHAR
+               Type = @Type, -- INT
+               Enable = @Enable, -- BIT
+               FromDate = @FromDate, -- DATETIME
+               ToDate = @ToDate -- DATETIME
+
+         WHERE dbo.[User].ID = @ID;
+         IF @Type = 3
+             BEGIN
+                 UPDATE dbo.Student
+                   SET 
+                       dbo.Student.FullName = @FullName, -- NVARCHAR
+                       dbo.Student.Gender = @Gender, -- TINYINT
+                       dbo.Student.DayOfBirth = @DayOfBirth, -- DATE
+                       dbo.Student.Address = @Address -- NVARCHAR
+                 --dbo.Student.MajorID = 0, -- INT
+                 WHERE dbo.Student.UserID = @ID; -- INT
+
+             END;
+     END;
+GO
--- ManageCourse/Admin/UCManageMajor.Designer.cs
+++ ManageCourse/Admin/UCManageMajor.Designer.cs
@@ -28,10 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblMessage = new System.Windows.Forms.Label();
             this.btnClear = new System.Windows.Forms.Button();
             this.nudNumberTopical = new System.Windows.Forms.NumericUpDown();
             this.btnSubmit = new System.Windows.Forms.Button();
@@ -39,22 +42,21 @@
             this.txtName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.lblMessage = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
             this.dgvMajor = new System.Windows.Forms.DataGridView();
-            this.btnDelete = new System.Windows.Forms.Button();
-            this.btnUpdate = new System.Windows.Forms.Button();
             this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.NameMajor = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Code = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.NumberTopical = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.TotalStudied = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nudNumberTopical)).BeginInit();
             this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMajor)).BeginInit();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -96,6 +98,17 @@
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thông tin";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblMessage.Location = new System.Drawing.Point(10, 171);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(11, 16);
+            this.lblMessage.TabIndex = 7;
+            this.lblMessage.Text = " ";
             // 
             // btnClear
             // 
@@ -166,42 +179,20 @@
             this.panel2.Size = new System.Drawing.Size(435, 377);
             this.panel2.TabIndex = 5;
             // 
-            // lblMessage
-            // 
-            this.lblMessage.AutoSize = true;
-            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMessage.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.lblMessage.Location = new System.Drawing.Point(10, 171);
-            this.lblMessage.Name = "lblMessage";
-            this.lblMessage.Size = new System.Drawing.Size(11, 16);
-            this.lblMessage.TabIndex = 7;
-            this.lblMessage.Text = " ";
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.btnUpdate);
-            this.panel3.Controls.Add(this.btnDelete);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(435, 39);
-            this.panel3.TabIndex = 8;
-            // 
             // dgvMajor
             // 
             this.dgvMajor.AllowUserToAddRows = false;
             this.dgvMajor.AllowUserToDeleteRows = false;
             this.dgvMajor.BackgroundColor = System.Drawing.Color.White;
             this.dgvMajor.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvMajor.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvMajor.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dgvMajor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvMajor.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ID,
@@ -209,41 +200,24 @@
             this.Code,
             this.NumberTopical,
             this.TotalStudied});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvMajor.DefaultCellStyle = dataGridViewCellStyle2;
             this.dgvMajor.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvMajor.GridColor = System.Drawing.Color.White;
             this.dgvMajor.Location = new System.Drawing.Point(0, 39);
             this.dgvMajor.Name = "dgvMajor";
             this.dgvMajor.ReadOnly = true;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgvMajor.RowsDefaultCellStyle = dataGridViewCellStyle3;
             this.dgvMajor.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvMajor.Size = new System.Drawing.Size(435, 338);
             this.dgvMajor.TabIndex = 9;
-            // 
-            // btnDelete
-            // 
-            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDelete.ForeColor = System.Drawing.Color.DarkGray;
-            this.btnDelete.Location = new System.Drawing.Point(362, 3);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(70, 31);
-            this.btnDelete.TabIndex = 5;
-            this.btnDelete.Text = "Xóa";
-            this.btnDelete.UseVisualStyleBackColor = true;
-            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
-            // 
-            // btnUpdate
-            // 
-            this.btnUpdate.BackColor = System.Drawing.Color.SteelBlue;
-            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUpdate.ForeColor = System.Drawing.Color.Snow;
-            this.btnUpdate.Location = new System.Drawing.Point(6, 3);
-            this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(87, 31);
-            this.btnUpdate.TabIndex = 6;
-            this.btnUpdate.Text = "Cập nhật";
-            this.btnUpdate.UseVisualStyleBackColor = false;
-            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // ID
             // 
@@ -285,6 +259,44 @@
             this.TotalStudied.ReadOnly = true;
             this.TotalStudied.Width = 120;
             // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnUpdate);
+            this.panel3.Controls.Add(this.btnDelete);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(435, 39);
+            this.panel3.TabIndex = 8;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.Color.Snow;
+            this.btnUpdate.Location = new System.Drawing.Point(6, 3);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(87, 31);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Cập nhật";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.DarkGray;
+            this.btnDelete.Location = new System.Drawing.Point(362, 3);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(70, 31);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
             // UCManageMajor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -300,8 +312,8 @@
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nudNumberTopical)).EndInit();
             this.panel2.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvMajor)).EndInit();
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
